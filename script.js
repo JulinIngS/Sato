4668c2c0793167616cf3c2328b7f51944731e164
JulioIngS <anaelmirey@gmail.com>
2023-04-29 13:08:14 -0500

    Sato

---
+++ script.js
@@ -0,0 +1,9 @@
+setTimeout(cambiaMensaje,4500)
+
+function cambiaMensaje(){
+    document.getElementById("loading-message").innerText="Abrazo Enviado a la chica más guapa! Osea Mi satory<3";
+}
+
+setTimeout(function () {
+    alert("Recibiste un abrazo virtual, Pero pronto recibiras uno presencial<3")
+},4550)
